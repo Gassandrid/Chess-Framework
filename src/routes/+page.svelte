66b0d745c9090264fe6748f37fc46b5e5052ff6b
sylvanfranklin@icloud.com <sylvanfranklin@ip0af5c22b.int.uvm.edu>
2024-10-09 12:28:08 -0400

    complete fronent rewrite step one

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,4 @@
 <script lang="ts">
-    import { invoke } from "@tauri-apps/api/tauri";
-
-    let current_fen =
-        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-
-    const board = Array(8).fill(Array(8).fill(null));
 </script>
 
-<div class="w-full h-full flex items-center justify-center mt-32">
-    <ol class="grid grid-rows-8 rounded-md overflow-clip">
-        {#each board as row, i}
-            <div class="flex">
-                {#each row as square, j}
-                    <div
-                        class={`h-20 w-20 flex items-center justify-center ${
-                            (i + j) % 2 === 0 ? "bg-gray-300" : "bg-gray-500"
-                        }`}
-                    >
-                    </div>
-                {/each}
-            </div>
-        {/each}
-    </ol>
-</div>
+<div class="w-full h-full flex items-center justify-center mt-32">restart</div>
